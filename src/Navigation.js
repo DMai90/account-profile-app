5ccf6fe38fd34637099c056c9d0e9f8733adf72b
David <dmai90@yahoo.com>
2018-12-25 15:51:47 -0800

    first commit

---
+++ src/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navigation = { username } => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container">
+        <h3>hi</h3>
+      </div>
+    </nav>
+  );
+};
+
+export default Navigation;
